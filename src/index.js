76653d6a8b70f6b3bb5aa42f7a1db5eb40d67149
Fathul Arifin <fyn.ifyn>
2024-12-02 14:22:48 +0200

    setting server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() =>
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `server is running on port ${process.env.PORT} and database connected `
+      );
+    })
+  )
+  .catch((error) => {
+    console.log(error);
+  });
