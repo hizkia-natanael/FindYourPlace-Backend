ab164074ca4e8f7f85e20e268b3579b6d3f8c3b5
Fathul Arifin <fyn.ifyn>
2024-12-02 14:12:07 +0200

    folder update

---
+++ src/models/placeModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const placeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  googleMapsLink: {
+    type: String,
+  },
+  images: {
+    type: [String],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Place = mongoose.model("Place", placeSchema);
+module.exports = Place;
