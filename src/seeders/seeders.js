cdf32d4a14eaa9db9135833f6b4ea8bca72db7ea
Hizkia Natanael <hizkianata1911gmail.com>
2024-12-09 00:59:53 +0700

    feat: nambahin data di seeder

--- src/seeders/seeders.js
+++ src/seeders/seeders.js
@@ -22,7 +22,62 @@ const users = [
     password: "user123",
     role: "user",
   },
+  {
+    username: "andika_123",
+    email: "andika@example.com",
+    password: "andika123",
+    role: "user",
+  },
+  {
+    username: "yuni_akbar",
+    email: "yuni@example.com",
+    password: "yuniakbar123",
+    role: "user",
+  },
+  {
+    username: "reza_gilang",
+    email: "reza@example.com",
+    password: "rezagilang123",
+    role: "user",
+  },
+  {
+    username: "sari_cahya",
+    email: "sari@example.com",
+    password: "saricahya123",
+    role: "user",
+  },
+  {
+    username: "hendra_putra",
+    email: "hendra@example.com",
+    password: "hendra123",
+    role: "user",
+  },
+  {
+    username: "melisa_sulistyo",
+    email: "melisa@example.com",
+    password: "melisa123",
+    role: "user",
+  },
+  {
+    username: "gilang_nugroho",
+    email: "gilang@example.com",
+    password: "gilang123",
+    role: "user",
+  },
+  {
+    username: "faisal_rahman",
+    email: "faisal@example.com",
+    password: "faisal123",
+    role: "user",
+  },
+  {
+    username: "diana_novianti",
+    email: "diana@example.com",
+    password: "diana123",
+    role: "user",
+  },
 ];
+
 
 const places = [
   {
@@ -39,7 +94,71 @@ const places = [
     googleMapsLink: "https://g.co/kgs/2d832s2",
     images: ["./photos/kupiku.jpg"],
   },
+  {
+    name: "Dapur Solo",
+    description:
+      "Dapur Solo menyajikan masakan khas Jawa dengan cita rasa yang autentik. Terletak di tengah kota Yogyakarta, tempat ini menjadi pilihan favorit bagi para pencinta kuliner yang ingin menikmati hidangan lezat dalam suasana yang nyaman.",
+    googleMapsLink: "https://g.co/kgs/mLZJ7A",
+    images: ["./photos/dapur_solo.jpg"],
+  },
+  {
+    name: "The Woke Up Cafe",
+    description:
+      "The Woke Up Cafe memiliki desain interior yang sangat instagramable dengan banyak spot foto menarik. Selain itu, kafe ini juga menyediakan menu-menu sehat dan kopi premium yang cocok untuk bersantai atau bekerja.",
+    googleMapsLink: "https://g.co/kgs/Fn53p4",
+    images: ["./photos/woke_up_cafe.jpg"],
+  },
+  {
+    name: "Angkringan Kopi Jos Mbah Lindu",
+    description:
+      "Angkringan Kopi Jos Mbah Lindu terkenal dengan sajian kopi jos-nya yang unik, dengan menggunakan arang panas dalam proses penyajiannya. Tempat ini cocok untuk kamu yang ingin menikmati suasana tradisional sambil menikmati makanan khas Yogyakarta seperti nasi kucing.",
+    googleMapsLink: "https://g.co/kgs/f55Lt4",
+    images: ["./photos/angkringan_mbah_lindu.jpg"],
+  },
+  {
+    name: "Sate Klathak Pak Pong",
+    description:
+      "Sate Klathak Pak Pong adalah tempat yang wajib dikunjungi bagi penggemar kuliner sate. Sate kambing yang dibakar dengan cara unik dan rasa bumbu khas membuat tempat ini selalu ramai pengunjung.",
+    googleMapsLink: "https://g.co/kgs/QgTKdz",
+    images: ["./photos/sate_klathak.jpg"],
+  },
+  {
+    name: "Merapi Coffee",
+    description:
+      "Merapi Coffee menawarkan pengalaman ngopi yang menyenangkan dengan pemandangan Gunung Merapi. Kafe ini cocok untuk bersantai sambil menikmati kopi lokal dan pemandangan alam yang menyejukkan.",
+    googleMapsLink: "https://g.co/kgs/kyk9Tp",
+    images: ["./photos/merapi_coffee.jpg"],
+  },
+  {
+    name: "Les Belles Yogyakarta",
+    description:
+      "Les Belles Yogyakarta adalah kafe dengan konsep French-style yang menawarkan suasana elegan dan santai. Tempat ini sangat cocok untuk bersantai sambil menikmati berbagai jenis pastry dan kopi berkualitas.",
+    googleMapsLink: "https://g.co/kgs/QJhTg5",
+    images: ["./photos/les_belles.jpg"],
+  },
+  {
+    name: "Taman Sari Water Castle",
+    description:
+      "Taman Sari Water Castle merupakan destinasi wisata bersejarah yang juga populer sebagai tempat nongkrong. Dengan arsitektur yang menawan dan suasana yang tenang, tempat ini sering digunakan untuk bersantai sambil menikmati pemandangan.",
+    googleMapsLink: "https://g.co/kgs/g9rHfZ",
+    images: ["./photos/taman_sari.jpg"],
+  },
+  {
+    name: "House of Raminten",
+    description:
+      "House of Raminten adalah restoran dan kafe yang menawarkan masakan tradisional Jawa dengan sentuhan modern. Tempat ini terkenal dengan konsep unik dan pelayanan yang ramah, serta menu khas yang menggugah selera.",
+    googleMapsLink: "https://g.co/kgs/vfhHMm",
+    images: ["./photos/house_of_raminten.jpg"],
+  },
+  {
+    name: "Cafe Brick",
+    description:
+      "Cafe Brick adalah kafe dengan konsep industrial yang nyaman dan instagramable. Menyajikan berbagai pilihan kopi dan makanan ringan, tempat ini cocok untuk nongkrong santai atau bekerja.",
+    googleMapsLink: "https://g.co/kgs/43mhLt",
+    images: ["./photos/cafe_brick.jpg"],
+  },
 ];
+
 
 const seedDatabase = async () => {
   try {
