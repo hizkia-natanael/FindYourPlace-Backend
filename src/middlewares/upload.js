88a624c22b53f81eb783bb3836e69f78a5712fc5
Hizkia Natanael <hizkianata1911gmail.com>
2024-12-19 16:43:38 +0700

    fixing placeroutes

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,6 +1,13 @@
 import cloudinary from 'cloudinary';
 import multer from 'multer';
 
+// Konfigurasi Cloudinary menggunakan variabel dari file .env
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 // Konfigurasi Multer untuk menangani file
 const storage = multer.memoryStorage();  // Menggunakan memory storage untuk mengirim file ke Cloudinary
 const upload = multer({ storage: storage }).single('image');  // Menangani satu file dengan field 'image'
